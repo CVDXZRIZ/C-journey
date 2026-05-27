e2a7c89384eb343fa65871315d5b6da51f2e5857
CVDXZRIZ <akmalfaiz694@gmail.com>
2026-05-27 08:38:00 +0700

    debloated oddsquare

--- oddsquaressum.c
+++ oddsquaressum.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 //this problem is only for registering into the euler project.
 //completely redundant
 int main (void){
@@ -8,7 +7,7 @@ long i;
 for (i=1; i<=180000; i+=2){
     b += i*i;
 }
-printf("===========================");
+printf("===========================\n");
 printf("thine answer is %lld\n", b);
 printf("===========================");
 return 0;
